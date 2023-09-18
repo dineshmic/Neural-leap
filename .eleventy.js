5e43f4bfd13bae84f06e645b742cc2e0ffaf7b18
dineshmic <dinesh.mic@gmail.com>
2023-09-18 23:49:12 +0530

    new2

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,10 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/previews');
     eleventyConfig.addPassthroughCopy('./src/admin');
+    eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/fonts');
+    eleventyConfig.addPassthroughCopy('./src/images');
+    eleventyConfig.addPassthroughCopy('./src/js');
 
     eleventyConfig.addCollection("blog", function(collection) {
         return collection.getFilteredByTag("blog");
